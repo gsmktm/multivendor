2f61c0662693050e148ad0991c486ccb19e1c2cf
surshrestha <surshrestha@growbydata.com>
2017-02-23 02:54:38 +0545

    rebirth

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/multivendor/admin/');
+define('HTTP_CATALOG', 'http://localhost/multivendor/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/multivendor/admin/');
+define('HTTPS_CATALOG', 'http://localhost/multivendor/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/multivendor/admin/');
+define('DIR_SYSTEM', '/var/www/html/multivendor/system/');
+define('DIR_IMAGE', '/var/www/html/multivendor/image/');
+define('DIR_LANGUAGE', '/var/www/html/multivendor/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/multivendor/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/multivendor/system/config/');
+define('DIR_CACHE', '/var/www/html/multivendor/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/multivendor/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/multivendor/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/multivendor/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/multivendor/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/html/multivendor/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'ven');
+define('DB_PASSWORD', 'vamps111`');
+define('DB_DATABASE', 'ven');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');

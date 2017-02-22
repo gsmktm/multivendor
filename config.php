ccd6f15421f8cc3633f2624b5b417b9ca47cad36
surshrestha <surshrestha@growbydata.com>
2017-02-23 02:06:37 +0545

    vqmod added for external changes

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/upload/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/upload/catalog/');
+define('DIR_SYSTEM', '/var/www/html/upload/system/');
+define('DIR_IMAGE', '/var/www/html/upload/image/');
+define('DIR_LANGUAGE', '/var/www/html/upload/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/upload/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/upload/system/config/');
+define('DIR_CACHE', '/var/www/html/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/upload/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/upload/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'suraj');
+define('DB_PASSWORD', 'vamps111`');
+define('DB_DATABASE', 'upload');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
